5c6ee03f1dbab6e9d3884482e36848d85b0dae49
TOVTC <veronica.tc.to@outlook.com>
2023-04-04 15:45:58 -0400

    components

--- main.js
+++ main.js
@@ -1,28 +1,84 @@
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: 
+    `
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+    `
+})
 
-var app = new Vue({
-    el: '#app',
-    data: {
-        brand: "Vue Mastery",
-        product: 'Socks',
-        selectedVariant: 0,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                onSale: false,
-                variantImage: './assets/images/vmSocks-green-onWhite.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                onSale: true,
-                variantImage: './assets/images/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 0
-            }
-        ],
-        cart: 0,
+Vue.component('product', {
+    // pass in an options object
+    // each component has its own scope and receives variables from its parent component using props (a custom attribute)
+    // in order to receive props, a component must explicitly declare props it expects to receive using the props option
+    // using a props object allows access to Vue's built-in validation options (e.g. dataType, required?, set a default value, etc.)
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: 
+    // templates can only have one root element, like in React, so wrap elements in a div
+    `
+    <div class="product">
+        <div class="product-image">
+            <img :src="image">
+        </div>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+
+            <product-details :details="details"></product-details>
+
+            <div v-for="(variant, index) in variants"
+                :key="variant.variantId"
+                class="color-box"
+                :style="{backgroundColor: variant.variantColor}"
+                @mouseover="updateProduct(index)"
+            >
+            </div>
+
+        <button v-on:click="addToCart" :disabled="!inStock" :class="{disabledButton: !inStock}">Add to Cart</button>
+        </div>
+
+        <div class="cart">
+            <p>Cart({{ cart }})</p>
+        </div>
+    </div>
+    `,
+    // data is not itself an object, but a function that returns an object so that each component that shares this data
+    // isn't sharing the same object but a new data object being returned every time
+    data() {
+        return {
+            brand: "Vue Mastery",
+            product: 'Socks',
+            selectedVariant: 0,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: './assets/images/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: './assets/images/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0,
+        }
     },
     methods: {
         addToCart() {
@@ -33,7 +89,6 @@ var app = new Vue({
         },
     },
     computed: {
-        // computed properties are cached - their values are saved until their dependencies are changed
         title() {
             return this.brand + ' ' + this.product
         },
@@ -43,10 +98,18 @@ var app = new Vue({
         inStock() {
             return this.variants[this.selectedVariant].variantQuantity
         },
-        onSale() {
-            if (this.variants[this.selectedVariant].onSale) {
-                return this.title + " is on sale!"
+        shipping() {
+            if (this.premium) {
+                return "Free"
             }
+            return 2.99
         }
+    }
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
     }
 })
