53517be8b755b3272f2b49009191f5194da29ddc
TOVTC <veronica.tc.to@outlook.com>
2023-04-04 14:02:25 -0400

    attribute binding

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-// creates new Vue instance - root of Vue application, pass in options object to store data and perform actions
-// Vue is reactive, so all data properties are linked to where that data is being referenced
+
 var app = new Vue({
-    // el property specifies which element to plug our app into (here, div with id app)
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'These are socks'
+        image: './assets/images/vmSocks-green-onWhite.jpg',
+        link: 'https://www.vuemastery.com/'
     }
 })
